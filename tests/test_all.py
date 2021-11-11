4f3ec003a1f65ff7685bba652fd4739688af2380
moonburnt <moonburnt@disroot.org>
2021-11-11 03:44:17 +0200

    feat: proper tests
    - Tests now have proper fixtures and work locally, without involving
    third party instance of hyscores. All network calls has been mocked.

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,68 +1,63 @@
 import pytest
 import hscp
-import string
-from random import randint, choices
-from os import environ
-from sys import exit
-from dotenv import load_dotenv
 
-load_dotenv(".env")
-
-url = environ["HSCP_TEST_URL"]
-app = environ["HSCP_TEST_APP"]
-
-login = None
-pw = None
-client = None
+url = "http://example.com"
+app = "hyscores"
+login = "asda"
+pw = "352354300n00"
+token = "324234efs42bt9ffon032r0frnd0fn"
 
 
-def _generate_string(length: int = 6) -> str:
-    return "".join(choices(string.ascii_uppercase + string.digits, k=length))
-
-
-def test_init():
-    global client
-    client = hscp.HyScoresClient(
+@pytest.fixture
+def client() -> hscp.HyScoresClient:
+    return hscp.HyScoresClient(
         url=url,
         app=app,
     )
 
 
-def test_token_requirement():
+@pytest.fixture
+def authorized_client(client):
+    client.token = token
+    return client
+
+
+def test_token_fail(client):
     with pytest.raises(hscp.TokenUnavailable):
         client.get_scores()
 
 
-def test_register():
-    global login
-    global pw
-
-    login = _generate_string()
-    pw = _generate_string()
-    client.register(login, pw)
+def test_register(requests_mock, client):
+    requests_mock.post(url + "/register", json={"result": True})
+    assert client.register(login, pw) is True
 
 
-def test_login():
+def test_login(requests_mock, client):
+    requests_mock.post(url + "/login", json={"result": {"token": token}})
     client.login(login, pw)
 
 
-def test_scores():
-    assert type(client.get_scores()) is list
+def test_scores(requests_mock, authorized_client):
+    requests_mock.get(url + "/scores", json={"result": []})
+    assert isinstance(authorized_client.get_scores(), list)
 
 
-def test_score():
-    assert type(client.get_score("sadam")) is dict
+def test_score(requests_mock, authorized_client):
+    requests_mock.get(url + "/score", json={"result": {"sadam": 36}})
+    assert isinstance(authorized_client.get_score("sadam"), dict)
 
 
-def test_score_fail():
+def test_score_fail(requests_mock, authorized_client):
+    requests_mock.get(url + "/score", json={"result": "Invalid Name"})
     with pytest.raises(hscp.InvalidName):
-        client.get_score("your mom")
+        authorized_client.get_score("your mom")
 
 
-def test_score_uploader():
-    assert client.post_score("sadam", randint(1, 100)) is True
+def test_score_uploader(requests_mock, authorized_client):
+    requests_mock.post(url + "/score", json={"result": True})
+    assert authorized_client.post_score("sadam", 69) is True
 
 
-def test_logout():
-    client.logout()
-    assert client.token is None
+def test_logout(authorized_client):
+    authorized_client.logout()
+    assert authorized_client.token is None
